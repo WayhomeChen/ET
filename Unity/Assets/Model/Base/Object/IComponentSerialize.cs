38f8755991d0859191c42d28b12b8115e7b11227
WayhomeChen <2360315747@qq.com>
2018-12-11 03:13:51 +0800

    给IComponentSerialize.cs和ISerializeToEntity.cs添加注释

--- Unity/Assets/Model/Base/Object/IComponentSerialize.cs
+++ Unity/Assets/Model/Base/Object/IComponentSerialize.cs
@@ -1,13 +1,19 @@
 ﻿namespace ETModel
 {
-	// 在序列化前或者反序列化之后需要做一些操作，可以实现该接口，该接口的方法需要手动调用
-	// 相比ISupportInitialize接口，BeginSerialize在BeginInit之前调用，EndDeSerialize在EndInit之后调用
-	// 并且需要手动调用，可以在反序列化之后，在次方法中将注册组件到EventSystem之中等等
+	/// <summary>
+	/// <para>
+	/// 在序列化前或者反序列化之后需要做一些操作，可以实现该接口，该接口的方法需要手动调用
+	/// </para><para>
+	/// 相比ISupportInitialize接口，BeginSerialize在BeginInit之前调用，EndDeSerialize在EndInit之后调用
+	/// </para><para>
+	/// 并且需要手动调用，可以在反序列化之后，在次方法中将注册组件到EventSystem之中等等
+	/// </para>
+	/// </summary>
 	public interface IComponentSerialize
 	{
-		// 序列化之前调用
+		/// <summary> 序列化之前调用 </summary>
 		void BeginSerialize();
-		// 反序列化之后调用
+		/// <summary> 反序列化之后调用 </summary>
 		void EndDeSerialize();
 	}
 }
--- Unity/Assets/Model/Base/Object/ISerializeToEntity.cs
+++ Unity/Assets/Model/Base/Object/ISerializeToEntity.cs
@@ -1,5 +1,8 @@
 ﻿namespace ETModel
 {
+	/// <summary>
+	/// 需要序列化到实体的接口（规则声明）
+	/// </summary>
 	public interface ISerializeToEntity
 	{
 	}
